b28db06e7f9ebd99e3a2e58d827e2b602d3e803a
Jarret Hoving <Jarret.ho@Hotmail.com>
2021-03-12 22:00:49 +0100

    Inserting Xcode

---
+++ DAC/DAC/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DAC
+//
+//  Created by Jarret hoving on 12/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
