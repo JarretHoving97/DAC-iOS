b13dd869ef4b17dad2bb7f3fc9c8e29534dbca81
Jarret Hoving <Jarret.ho@Hotmail.com>
2021-03-19 17:52:39 +0100

    Default snackbar/pop-up added.

---
+++ DAC/DAC/JaHoLibrary/JaHoPops/JaHoPops.swift
@@ -0,0 +1,20 @@
+//
+//  JaHoPops.swift
+//  DAC
+//
+//  Created by Jarret hoving on 18/03/2021.
+//
+
+import SwiftUI
+
+struct JaHoPops: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct JaHoPops_Previews: PreviewProvider {
+    static var previews: some View {
+        JaHoPops()
+    }
+}
